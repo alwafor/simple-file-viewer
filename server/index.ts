9ac8999ab7c4f3823a6453cf879fc204a4efc14e
alwafor <alwafor1@gmail.com>
2022-12-27 17:56:01 +0300

    create simple node server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world!');
+import http from 'http'
+
+const PORT = 5000
+
+const server = http.createServer((req, res) => {
+  res.setHeader('access-control-allow-origin', '*')
+
+  if (req.url === '/') {
+    res.writeHead(200)
+    res.end("Hello there!")
+    return
+  }
+
+  res.writeHead(404)
+  res.end("This route doesn't exist!")
+})
+
+server.listen(PORT, () => console.log(`server is running on ${PORT}`))
